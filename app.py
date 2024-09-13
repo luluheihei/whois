002144bcbfb3a4a8ba0e3a8046fa6e9d683d89cc
luluheihei <chyy3539@gmail.com>
2024-09-13 18:36:44 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+# ngrok에서 토큰을 가져옵니다. 2bU0qTkJTVSo8YOckEG6eN7ZxMf_6ogvHp9x3ZDT7i3ZC4pFb
+# 토근 설정을 합니다.
+from pyngrok import ngrok
+ngrok.set_auth_token('2lmWSjXp4Qt8Rp5OHo3xRoLxO8j_6hU3GZh6BXwjEXgBnpsn1')
+#엔그록 토큰: 2lmWSjXp4Qt8Rp5OHo3xRoLxO8j_6hU3GZh6BXwjEXgBnpsn1
+
+import streamlit as st
+from keras.models import load_model
+from PIL import Image, ImageOps  
+import numpy as np
+
+# 파일 업로더를 만듭니다.
+file = st.file_uploader('이미지를 업로드해주세요', type=['jpg','png'])
+
+np.set_printoptions(suppress=True)
+
+# 모델을 가져옵니다.
+model = load_model("keras_Model.h5", compile=False)
+
+# 라벨을 읽습니다.
+class_names = open("labels.txt", "r", encoding='UTF-8').readlines()
+
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+if file is None:
+  st.text('이미지를 업로드하세요')
+else:   
+    # 파일을 엽니다.
+    image = Image.open(file).convert("RGB")
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.Resampling.LANCZOS)
+    
+    image_array = np.asarray(image)  
+    
+    normalized_image_array = (image_array.astype(np.float32) / 127.5) - 1
+   
+    data[0] = normalized_image_array
+    st.image(image, use_column_width = True)
+    prediction = model.predict(data)
+    index = np.argmax(prediction)
+    # class_name는 1 고양이와 같이 문자열입니다.
+    class_name = class_names[index]
+    confidence_score = prediction[0][index]  
+    confidence_score = round(confidence_score*100, 2)
+    st.text(f'{class_name.split(" " )[1]} {confidence_score}%')
+
